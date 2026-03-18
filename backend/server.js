50fc977627aa3a523c9fe2af8c34982f79b29bd6
Tom <zuckm709@gmail.com>
2026-03-17 19:25:21 -0500

    implement self manage

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,6 @@ import morgan from "morgan";
 import { connectDB } from "./utils/db.js";
 import routes from "./routes/index.js";
 import path from "path";
-import { exec } from "child_process";
-import util from "util";
 
 dotenv.config();
 
@@ -34,24 +32,6 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use("/api", routes);
 
-const execAsync = util.promisify(exec);
-
-async function main() {
-  try {
-    console.log("Applying pending Prisma migrations...");
-
-    // Run Prisma migration command programmatically
-    const { stdout, stderr } = await execAsync("npx prisma migrate deploy");
-    console.log(stdout);
-    if (stderr) console.error(stderr);
-  } catch (err) {
-    console.error("Migration failed:", err);
-  }
-
-  // Start your server after migrations
-  // e.g., app.listen(10000, () => console.log("Server running..."));
-}
-
 // SPA fallback (IMPORTANT)
 app.use(express.static(path.join(__dirname, "backend", "dist")));
 
@@ -61,8 +41,6 @@ app.get("*", (req, res) => {
 
 // app.use(routeNotFound);
 // app.use(errorHandler);
-
-main();
 
 app.listen(port, () =>
   console.log(`Server listening on http://localhost:${port}`),
